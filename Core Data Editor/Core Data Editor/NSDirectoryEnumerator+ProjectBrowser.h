18b8425fb06005445574c05f2d2f3cddea60f291
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2018-03-21 00:01:33 +0900

    Improve performance when reloading projects with supporting latest iOS Simulator.

--- Core Data Editor/Core Data Editor/NSDirectoryEnumerator+ProjectBrowser.h
+++ Core Data Editor/Core Data Editor/NSDirectoryEnumerator+ProjectBrowser.h
@@ -1,6 +1,16 @@
 #import <Foundation/Foundation.h>
 
+@interface NSURL (ProjectBrowser)
+
+- (nullable NSManagedObjectModel *)transformedManagedObjectModel;
+
+@end
+
 @interface NSDirectoryEnumerator (ProjectBrowser)
+
+- (void)getModeldata:(NSDictionary<NSString*, NSManagedObjectModel*> **)outModelByModelPath;
+
+- (void)getMetadata:(NSDictionary<NSString*, NSDictionary*> **)outMetadataByStorePath;
 
 - (void)getMetadataByStorePath:(NSDictionary<NSString*, NSDictionary*> **)metadataByStorePath modelByModelPath:(NSDictionary<NSString*, NSManagedObjectModel*> **)modelByModelPath;
 
